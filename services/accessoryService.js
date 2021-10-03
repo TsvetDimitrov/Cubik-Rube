da19a229c5dfd578bccdb0c783561a7b4acf0a12
Tsvetomir <cvetodimitrov@tu-sofia.bg>
2021-10-03 15:01:32 +0300

    Attach filtered accessories

--- services/accessoryService.js
+++ services/accessoryService.js
@@ -4,14 +4,18 @@ async function create(name, description, imageUrl) {
     return Accessory.create({name, description, imageUrl});
 }
 
-
 async function getAll(){
     return Accessory.find({}).lean();
+}
+
+async function getAllWithout(accesoryIds){
+    return Accessory.find({_id: {$nin: accesoryIds}}).lean();
 }
 
 const accessoryService = {
     getAll,
     create,
+    getAllWithout
 }
 
 module.exports = accessoryService;
