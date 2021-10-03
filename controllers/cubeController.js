d635c4dfefa2ddea35797297391e557833d7cb7d
Tsvetomir <cvetodimitrov@tu-sofia.bg>
2021-10-03 14:11:23 +0300

    adapted controllers for the new schema

--- controllers/cubeController.js
+++ controllers/cubeController.js
@@ -1,29 +1,32 @@
-const express = require('express');
+const router = require('express').Router();
 
 const cubeService = require('../services/cubeService');
 
-const router = express.Router();
 
 const getCreateCubePage = (req, res) => {
-    let cubes = cubeService.getAll();
-    console.log(cubes);
 
     res.render('create');
 };
 
-const createCube = (req, res) => {
-    let {name, description, imageUrl, difficulty} = req.body;
+const createCube = async (req, res) => {
+    let { name, description, imageUrl, difficulty } = req.body;
 
-    cubeService.create(name, description, imageUrl, difficulty);
 
-    res.redirect('/');
+    try {
+        await cubeService.create(name, description, imageUrl, difficulty);
+
+        res.redirect('/');
+
+    }catch(err){
+        res.status(400).send(err.message).end();
+    }
 }
 
 
-const cubeDetails = (req, res) => {
-    let cube = cubeService.getOne(req.params.cubeId);
+const cubeDetails = async (req, res) => {
+    let cube = await cubeService.getOne(req.params.cubeId);
 
-    res.render('details', {...cube});
+    res.render('details', { ...cube });
 }
 
 
