1922e1a0db1c6c2751f5f4d459efa6c036659fc2
Tsvetomir <cvetodimitrov@tu-sofia.bg>
2021-10-10 14:36:45 +0300

    Add login post action

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -5,5 +5,10 @@ router.get('/login', (req, res) => {
     res.render('auth/login');
 });
 
+router.post('/login', (req, res) => {
+    console.log(req.body);
+    res.redirect('/login');
+});
+
 
 module.exports = router;
